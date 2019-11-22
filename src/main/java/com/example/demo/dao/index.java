b82735d78a8abfc3809b1faa143f637667a97d67
sys <sys@qq.com>
2019-11-22 18:51:42 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/dao/index.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+public class index {
+      public  void  index(){
+          System.out.println("aaaaaaaaaaaa");
+          System.out.println("aaaaaaaaaaaa");
+          System.out.println("aaaaaaaaaaaa");
+          System.out.println("aaaaaaaaaaaa");
+
+      }
+}
